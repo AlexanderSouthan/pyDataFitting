51eb1fc11e23a90695d2c0517e5334fade403245
Alexander Southan <72788772+AlexanderSouthan@users.noreply.github.com>
2021-10-18 16:10:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pyRegression',
     version='0.0.1',
-    packages=find_packages(where='src')
+    packages=find_packages(where='src'),
+    install_requires=['numpy', 'pandas', 'scipy', 'numbers', 'matplotlib', 'scikit-learn', 'statsmodels'],
+    # dependency_links=['http://github.com/user/repo/tarball/master#egg=package-1.0']
 )
