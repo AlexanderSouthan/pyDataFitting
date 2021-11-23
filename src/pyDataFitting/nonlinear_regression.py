4ad8cbdb7f9b691d9a849b8eb1ba210d6c72aab6
AlexanderSouthan <72788772+AlexanderSouthan@users.noreply.github.com>
2021-11-23 21:15:28 +0100

    Added different optimization methods

--- src/pyDataFitting/nonlinear_regression.py
+++ src/pyDataFitting/nonlinear_regression.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from scipy.optimize import differential_evolution, least_squares
+from scipy.optimize import (
+    differential_evolution, least_squares, basinhopping, brute, shgo,
+    dual_annealing)
 from scipy import integrate
 from scipy.stats import gaussian_kde
 
@@ -14,12 +16,60 @@ from little_helpers.math_functions import (
 # 2D nonlinear regression
 #####################################
 
-def nonlinear_regression(x_values, y_values, function_type, z_values=None,
-                         boundaries=None, initial_guess=None, max_iter=1000,
-                         alg='evo', weights=None, **kwargs):
-    assert alg in ['evo', 'lm'], ('No valid alg. Allowed values are \'%s\' '
-                                  'and \'%s\'') % ('evo', 'lm')
+def nonlinear_regression(x_values, y_values, function_type, alg='evo',
+                         **kwargs):
+    """
+    Do a nonlinear regression on a dataset.
 
+    The objective function to be minimized is the sum of the squared residuals.
+
+    Parameters
+    ----------
+    x_values : ndarray
+        A 1D array containing the independent variable.
+    y_values : ndarray
+        A 1D array containing the dependent variable. Must contain the same
+        number of elements like x_values.
+    function_type : string
+        The function to be fitted to the data. Must be a valid function type
+        for calc_function below.
+    alg : string, optional
+        The algorithm used to minimize the sum of the squared residuals.
+        Allowed values are 'evo' for differential evolution, 'lm' for
+        Levenberg-Marquardt. The default is 'evo'.
+    **kwargs :
+        weights : 'string' or None, optional
+            Defines the method of weight calculation. Can be 'kde'
+            (weights determined by the kernel density estimate) or
+            'inverse_y' (weights are the inverse of y_values). Default is
+            None, meaning that all weights are equal.
+        for alg in ['evo', 'brute', 'shgo', 'dual_annealing']:
+            boundaries : list of tuples
+                A list with tuples containing two elements each, giving the
+                upper and lower boundaries for each parameter.
+        for alg == 'evo' or alg == 'dual_annealing':
+            max_iter : int, optional
+                The maximum numer of iterations used during optimization. The
+                default is 1000.
+        for alg == 'brute':
+            grid_points : int, optional
+                The number of grid points along an axis. Default is 20.
+        for alg == 'lm' and alg == 'basinhopping':
+            initial_guess : array_like
+                Initial guess of the parameters to be found.
+        for alg == 'basinhopping':
+            n_iter : int, optional
+                The number of iterations. The default is 100.
+
+    Returns
+    -------
+    OptimizeResult
+        The object containing the optimization result.
+
+    """
+    algs = ['evo', 'lm', 'basinhopping', 'brute', 'shgo', 'dual_annealing']
+
+    weights = kwargs.get('weights', None)
     if weights is None:
         weights = np.ones_like(x_values)
     elif weights == 'kde':  # kernel density estimation
@@ -29,19 +79,82 @@ def nonlinear_regression(x_values, y_values, function_type, z_values=None,
     elif weights == 'inverse_y':
         weights = 1/y_values
 
-    if alg == 'evo':
+    if alg == algs[0]:  # 'evo'
+        boundaries = kwargs.get('boundaries', None)
+        max_iter = kwargs.get('max_iter', 1000)
         return differential_evolution(
-            fit_error, bounds=boundaries,
-            args=(x_values, y_values, function_type, weights, 'sum_of_squares'),
+            fit_error, bounds=boundaries, args=(
+                x_values, y_values, function_type, weights, 'sum_of_squares'),
             maxiter=max_iter)
-    if alg == 'lm':
+
+    elif alg == algs[1]:  # 'lm'
+        initial_guess = kwargs.get('initial_guess', None)
         return least_squares(
             fit_error, initial_guess,
-            args=(x_values, y_values, function_type, 'residuals'), method='lm')
+            args=(x_values, y_values, function_type, weights, 'residuals'),
+            method='lm')
 
+    elif alg == algs[2]:  # 'basinhopping'
+        initial_guess = kwargs.get('initial_guess', None)
+        n_iter = kwargs.get('n_iter', 100)
+        return basinhopping(fit_error, initial_guess, minimizer_kwargs={
+            'args':(x_values, y_values, function_type, weights, 'sum_of_squares')},
+            niter=n_iter)
+
+    elif alg == algs[3]:  # 'brute'
+        boundaries = kwargs.get('boundaries', None)
+        grid_points = kwargs.get('grid_points', 20)
+        return brute(
+            fit_error, ranges=boundaries, args=(
+                x_values, y_values, function_type, weights, 'sum_of_squares'),
+            Ns=grid_points)
+
+    elif alg == algs[4]:  # 'shgo'
+        boundaries = kwargs.get('boundaries', None)
+        return shgo(
+            fit_error, bounds=boundaries, args=(
+                x_values, y_values, function_type, weights, 'sum_of_squares'))
+
+    elif alg == algs[5]:  # 'dual_annealing'
+        boundaries = kwargs.get('boundaries', None)
+        max_iter = kwargs.get('max_iter', 1000)
+        return dual_annealing(
+            fit_error, bounds=boundaries, maxiter=max_iter, args=(
+                x_values, y_values, function_type, weights, 'sum_of_squares'))
+
+    else:
+        raise ValueError('No valid alg. Allowed values must be in {}.'.format(
+            algs))
 
 def fit_error(fit_par, x_values, y_values, function_type, weights,
               mode='sum_of_squares'):
+    """
+    The objective function to be minimized with nonlinear_regresssion.
+
+    Parameters
+    ----------
+    fit_par : list
+        A list containing the current values of the fit parameters.
+    x_values : ndarray
+        See docstring of nonlinear_regression.
+    y_values : ndarray
+        See docstring of nonlinear_regression.
+    function_type : string
+        See docstring of nonlinear_regression.
+    weights : ndarray
+        An array containing the weights of the different data points. Must
+        contain as many elements as x_values.
+    mode : string, optional
+        Determines if the sum of squares of the residuals is returned
+        ('sum_of_squares') or the residuals themselves ('residuals'). The
+        default is 'sum_of_squares'.
+
+    Returns
+    -------
+    float or ndarray
+        The sum of squared residuals or the residuals, dpeneding on mode.
+
+    """
     curr_values = calc_function(x_values, fit_par, function_type)
 
     modes = ['sum_of_squares', 'residuals']
