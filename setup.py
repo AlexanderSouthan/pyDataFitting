a96154fc9a8b9dddcfd45e8666b8f9732581cafd
AlexanderSouthan <72788772+AlexanderSouthan@users.noreply.github.com>
2022-08-01 20:50:51 +0200

    increased version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyDataFitting',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(where='src'),
     install_requires=['numpy', 'pandas', 'scipy', 'matplotlib', 'scikit-learn', 'statsmodels', 'little_helpers'],
 )
