4b523faa2d541fe5bb6e3b939738d349f6a7a175
Alexander Southan <72788772+AlexanderSouthan@users.noreply.github.com>
2023-07-25 12:46:19 +0200

    Included some imports

--- src/pyDataFitting/__init__.py
+++ src/pyDataFitting/__init__.py
@@ -2,6 +2,7 @@
 from . import linear_regression
 from .principle_component_regression import principal_component_regression
 from .partial_least_squares_regression import pls_regression
-from . import nonlinear_regression
+from .nonlinear_regression import nonlinear_regression
 from . import polynomial_regression
 from .model_tools import model_tools
+from .model_diagnostics import theo_residual_percentiles, percentiles
